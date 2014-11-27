f6c1fb1b38d897136c3c28a63bad097c0d2a70d9
Plinio Theodoro <pliniotheodoro@gmail.com>
2014-11-27 17:34:51 -0200

    Adicionado Browser Sync

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@
         less = require('gulp-less'),
         concat = require('gulp-concat'),
         rename = require('gulp-rename'),
-        cssmin = require('gulp-cssmin')
+        cssmin = require('gulp-cssmin'),
         imagemin = require('gulp-imagemin'),
-        pngquant = require('imagemin-pngquant');
+        pngquant = require('imagemin-pngquant'),
+        browserSync = require('browser-sync');
 
-    var filescss = "./assets/css/**/*.less",
+    var filescss = "./assets/css/site/*.less",
         filesimages = "./assets/img/images/*";
 
     gulp.task('less', function () {
@@ -29,9 +30,17 @@
          .pipe(gulp.dest('./assets/img/site/'));
         });
 
+    gulp.task('browser-sync', function() {
+        browserSync(["./assets/css/**/*.css", "./html/*.html"], {
+        server: {
+                baseDir: "./"
+                }
+            });
+        });
+
     gulp.task('default', function() {
             
-        gulp.run('less', 'images');
+        gulp.run('less', 'images', 'browser-sync');
 
         gulp.watch(filescss, ['less']);
 
